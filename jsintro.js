5ca9bf3d5067453f561bb1e9f88307771cd12fe8
Your Name <you@example.com>
2024-09-18 22:17:44 +0530

    added all files

---
+++ jsintro.js
@@ -0,0 +1,30 @@
+console.log("sruthi")
+console.log("a")
+console.log(41233)
+var a=12;
+
+
+
+var age={
+    age:15,
+}
+if (age.age=11){
+    console.log(false);
+    
+}
+else{
+    console.log(true);
+    
+}
+var Name="sruthi";
+var age=20;
+console.log(Name);
+console.log(age);
+var bluetooth={
+    boat:"black",
+    noise:"green",
+    oneplus:"blue",
+}
+console.log(bluetooth.noise);
+
+
